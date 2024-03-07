b68d5c817e06af84372888c04deff14ca65e7472
Felipe <felipematias96@gmail.com>
2024-03-07 20:07:41 -0300

    atualizacao agg

--- models/marts/agg_salesperson_territory.sql
+++ models/marts/agg_salesperson_territory.sql
@@ -1,7 +1,7 @@
 with
     salesperson as (
         select 
-            sk_salesperson
+            sk_salesperson as fk_salesperson
             , businessentityid as salespersonid
             , full_name
         from {{ref('dim_salesperson')}}
@@ -9,7 +9,7 @@ with
 
     , territory as (
         select 
-            sk_territory
+            sk_territory  as fk_territory
             , territoryid
             , territory_name
         from {{ref('dim_territory')}}
@@ -27,23 +27,23 @@ with
 
     , salesperson_territory as (
         select
-            territory.territoryid
+            territory.fk_territory
             , territory.territory_name
-            , salesperson.salespersonid
+            , salesperson.fk_salesperson
             , salesperson.full_name
             , round(sum(sales_order.order_qty), 2) as total_products_amount
             , round(sum(sales_order.unit_price), 2) as total_salesperson_due
             , round(sum(sales_order.unit_price) / sum(sales_order.order_qty), 2) as average_order_value
         from sales_order
-        left join salesperson on salesperson.sk_salesperson = sales_order.fk_salesperson
-        left join territory on territory.sk_territory = sales_order.fk_territory
-        where salesperson.sk_salesperson is not null
-        group by territory.territory_name, salesperson.full_name, territory.territoryid, salesperson.salespersonid
+        left join salesperson on salesperson.fk_salesperson = sales_order.fk_salesperson
+        left join territory on territory.fk_territory = sales_order.fk_territory
+        where salesperson.fk_salesperson is not null
+        group by territory.territory_name, salesperson.full_name, territory.fk_territory, salesperson.fk_salesperson
     )
 
     , agg_salesperson_territory as (
         select
-            {{dbt_utils.generate_surrogate_key(['territoryid', 'salespersonid'])}} as sk_salesperson_territory
+            {{dbt_utils.generate_surrogate_key(['fk_territory', 'fk_salesperson'])}} as sk_salesperson_territory
             , *
         from salesperson_territory
     )
